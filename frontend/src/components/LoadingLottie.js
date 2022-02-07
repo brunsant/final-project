db5779f1f0f1ffbdf4e3732e1b9f8783779ed1ac
Emelie <Emelie@Jophees-MacBook-Pro.local>
2022-02-07 18:36:53 +0100

    Add: Lottie animation and images

---
+++ frontend/src/components/LoadingLottie.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Lottie from "react-lottie"
+import animationData from "../Animations/loading"
+
+// import "./loadingindicator.css"
+
+const LoadingLottie = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  }
+
+  return (
+    <div className="lottie-container">
+      <div className="lottie">
+        <Lottie options={defaultOptions} height={500} width={500} />
+      </div>
+    </div>
+  )
+}
+export default LoadingLottie
