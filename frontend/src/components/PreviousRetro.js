967f10ec00100125a1d7bcf0c0e0e35edbd77692
Bruna Santos Araujo <brunasantosaraujo@Brunas-Air.lan>
2022-02-14 15:52:15 +0100

    test: backend user information

--- frontend/src/components/PreviousRetro.js
+++ frontend/src/components/PreviousRetro.js
@@ -18,7 +18,7 @@ const PreviousRetro = () => {
   console.log("USER ID PREVIOUS RETRO", userId);
 
   useEffect(() => {
-    fetch(RETRO_URL(`${userId}`))
+    fetch(RETRO_URL(`${userId._id}`))
       .then((res) => res.json())
       // eslint-disable-next-line no-sequences
       .then((data) => (setUserRetro(data.response), setRetroId(data.response)));
